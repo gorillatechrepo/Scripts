dd9cfb64847e0867f3fcf3d5e3e2c00d390c6da3
thekidla <l.peele@gmail.com>
2023-03-12 06:36:39 -0500

    Update M365_EmailSecurityCheck.ps1
    Added Save Dialog and Only output the missing records

--- M365_EmailSecurityCheck.ps1
+++ M365_EmailSecurityCheck.ps1
@@ -20,30 +20,34 @@ $missingRecords = @()
 
 # Check DKIM & DMARC
 Write-Output "-------- DKIM and DMARC DNS Records Report --------`n"
-
+$Result = "Protection`tDomain`tTyp`tHost name`tValue`tTTL`n"
 foreach ($Domain in $Domains) {
     # Check DKIM Selector 1 CNAME Record
     $dkimselector1 = nslookup -q=cname selector1._domainkey.$Domain 2> $null | Select-String "canonical name"
     if (!$dkimselector1) {
         $missingRecords += "DKIM Selector 1 for $Domain"
+        $Result += "DKIM`t$Domain`tCNAME`tselector1._domainkey`tselector1-$($Domain -replace "\.", "-")._domainkey.$TenantName`t3600`n"
     }
 
     # Check DKIM Selector 2 CNAME Record
     $dkimselector2 = nslookup -q=cname selector2._domainkey.$Domain 2> $null | Select-String "canonical name"
     if (!$dkimselector2) {
         $missingRecords += "DKIM Selector 2 for $Domain"
+        $Result += "DKIM`t$Domain`tCNAME`tselector2._domainkey`tselector2-$($Domain -replace "\.", "-")._domainkey.$TenantName`t3600`n"
     }
 
     # Check DMARC TXT Record
     $dmarc = (nslookup -q=txt _dmarc.$Domain 2> $null | Select-String "DMARC1") -replace "`t", ""
     if (!$dmarc) {
         $missingRecords += "DMARC for $Domain"
+        $Result += "DMARC`t$Domain`tTXT`t_dmarc`tv=DMARC1; p=none; pct=100; rua=mailto:$ReportMailbox; ruf=mailto:$ReportMailbox; fo=1`t3600`n"
     }
 
     # Check SPF TXT Record
     $spf = (nslookup -q=txt $Domain 2> $null | Select-String "v=spf1") -replace "`t", ""
     if (!$spf) {
         $missingRecords += "SPF for $Domain"
+        $Result += "SPF`t$Domain`tTXT`t@`tv=spf1 include:spf.protection.outlook.com -all`t3600`n"
     }
 
    # Output results for current domain
@@ -119,23 +123,26 @@ if ($missingRecords.Count -gt 0) {
         # Create order text for DKIM and DMARC records.
     $ReportMailbox = Read-Host = "What is the reporting mailbox address for DMARC? (Ex:report@domain.com)"
     #$TenantName = "example.onmicrosoft.com"
-    
-
-$Result = "Protection`tDomain`tTyp`tHost name`tValue`tTTL`n"
-
-foreach ($Domain in $Domains) {
-    $Result += "SPF`t$Domain`tTXT`t@`tv=spf1 include:spf.protection.outlook.com -all`t3600`n"
-    $Result += "DKIM`t$Domain`tCNAME`tselector1._domainkey`tselector1-$($Domain -replace "\.", "-")._domainkey.$TenantName`t3600`n"
-    $Result += "DKIM`t$Domain`tCNAME`tselector2._domainkey`tselector2-$($Domain -replace "\.", "-")._domainkey.$TenantName`t3600`n"
-    $Result += "DMARC`t$Domain`tTXT`t_dmarc`tv=DMARC1; p=none; pct=100; rua=mailto:$ReportMailbox; ruf=mailto:$ReportMailbox; fo=1`t3600`n"
-
-    
-}
-        Write-Output "CSV file generated and loaded to your clipboar. Open Excel and hit Ctrl+V"
-    } else {
+    Write-Output "CSV file generated and loaded to your clipboar. Open Excel and hit Ctrl+V"
+    } else {c
         Write-Output "No CSV file generated."
     }
 } else {
     Write-Output "All necessary DNS records exist."
 }
-$Result | Clip
+
+
+# Prompt the user to select a location to save the CSV file
+$saveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+$saveFileDialog.Title = "Save CSV file"
+$saveFileDialog.Filter = "CSV files (*.csv)|*.csv"
+
+if ($saveFileDialog.ShowDialog() -ne [System.Windows.Forms.DialogResult]::OK) {
+    Write-Host "No file selected"
+    return
+}
+
+# Save the Missing DNS Records to the selected CSV file
+$Result | ConvertFrom-Csv -Delimiter "`t" | Export-Csv $saveFileDialog.FileName -NoTypeInformation
+Write-Host "DNS records saved to '$($saveFileDialog.FileName)'"
+Write-Host "Done."
